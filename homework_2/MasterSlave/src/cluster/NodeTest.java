93824e2e4da9bfcbcff42c32466fc5d3191d1201
SergioCSC <zaregil@gmx.com>
2012-10-22 17:14:31 +0400

    all the cluster, but only local, lightly tested

--- homework_2/MasterSlave/src/cluster/NodeTest.java
+++ homework_2/MasterSlave/src/cluster/NodeTest.java
@@ -2,6 +2,7 @@ package cluster;
 
 import junit.framework.TestCase;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
@@ -17,13 +18,13 @@ public class NodeTest extends TestCase {
     public void testCrud() throws IOException, DataFormatException, NoSuchAlgorithmException {
         Cluster cluster = new Cluster(3);
         Node masterNode = new MasterNode(cluster, 3);
-        try (Scanner in = new Scanner("commands_1.txt")) {
+        try (Scanner in = new Scanner(new FileReader("test_1.txt"))) {
             while (in.hasNext()) {
                 String result = masterNode.crud(in.nextLine());
                 System.out.println(result);
             }
         }
-        try (Scanner in = new Scanner("commands_2.txt")) {
+        try (Scanner in = new Scanner(new FileReader("test_2.txt"))) {
             while (in.hasNext()) {
                 masterNode.crud(in.nextLine());
                 String result = masterNode.crud(in.nextLine());
