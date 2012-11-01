d88b13bae2bc4de320cad6a79054179950544dce
Artemii <achugr@yandex-team.ru>
2012-11-01 14:26:47 +0400

    + test

--- src/test/java/ru/csc/database/core/HashBaseTest.java
+++ src/test/java/ru/csc/database/core/HashBaseTest.java
@@ -3,7 +3,7 @@ package ru.csc.database.core;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 import java.security.NoSuchAlgorithmException;
 
 /**
@@ -11,23 +11,41 @@ import java.security.NoSuchAlgorithmException;
  *         30.10.12
  */
 public class HashBaseTest {
+    private static final DBRecord DB_RECORD = new DBRecord("123", new String[]{"artemij", "chugreev"});
 
     @Test
     public void testAddCommand() throws NoSuchAlgorithmException, UnsupportedEncodingException {
         {
             HashBase hashBase = new HashBase(10);
-            DBRecord dbRecord = new DBRecord("123", new String[]{"artemij", "chugreev"});
-            hashBase.create(dbRecord);
+            hashBase.create(DB_RECORD);
             DBRecord retrievedDbRecord = hashBase.retrieve("123");
-            Assert.assertEquals(dbRecord, retrievedDbRecord);
+            Assert.assertEquals(DB_RECORD, retrievedDbRecord);
         }
         {
             HashBase hashBase = new HashBase();
-            DBRecord dbRecord = new DBRecord("123", new String[]{"artemij", "chugreev"});
-            hashBase.create(dbRecord);
+            hashBase.create(DB_RECORD);
             DBRecord retrievedDbRecord = hashBase.retrieve("123");
-            Assert.assertEquals(dbRecord, retrievedDbRecord);
+            Assert.assertEquals(DB_RECORD, retrievedDbRecord);
         }
+    }
+
+    @Test
+    public void flushLoadTest() throws NoSuchAlgorithmException, IOException, ClassNotFoundException {
+        final HashBase hashBase = new HashBase();
+        final String surname = "chugreev";
+        final String telNumber = "123";
+//        add record
+        ConsoleApp.perform("add(" + surname + "," + telNumber + ")", hashBase, new PrintWriter(System.out));
+//        flush base
+        ConsoleApp.perform("flush 1", hashBase, new PrintWriter(System.out));
+//        exit
+        ConsoleApp.perform("exit", hashBase, new PrintWriter(System.out));
+
+        final HashBase loadedHashBase = new HashBase();
+//        load base
+        ConsoleApp.perform("load 1", loadedHashBase, new PrintWriter(System.out));
+//        base should contain my record
+        Assert.assertNotNull(loadedHashBase.retrieve(surname));
     }
 
 }
